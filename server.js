1db2459c1d00d5c7a157f3c53bc8c4371bb84485
byumvuhore <byumvuhorelucien@gmail.com>
2023-11-15 07:17:08 -0800

    BLB

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-//this is where your express sever is created and managed
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-//do not forget to import the necessary modules,
+// Connect to MongoDB
+mongoose.connect(mongodb+srv://byumvuhorelucien:byumv2008@cluster0.8p6bbyo.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection Error: '))
+db.once('open', function () {
+    console.log('Connected successfully  to  Database  SALES!');
+});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//do not forget to connect to your mongoDb
+6// Define routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
-
-
-//do not forget to set up routes to serve out the html file
-
-// Do not forget to handle your form posts for the relevant forms
-
-// Do not forget to handle form gets for the necessary data to display on the user interface.
-
-
-//Do not forget to catch all the erros that may arise during the process of parsing data from one form to another.
-
-// do not forget to create a port for your application and serving out the appliacation via the port created 
